ef2a61d89e12064e6aa48fb92b87db1194111677
jeru523 <atandijerusa@gmail.com>
2023-02-23 18:21:57 +0300

    Printing numbers from 0-9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,10 +5,10 @@
  */
 void print_numbers(void)
 {
-	int num;
+	char c;
 
-	for (num = 0; num <= 9; num++)
-		_putchar((num % 10) + '0');
+	for (c = '0'; c <= 9; c++)
+	_putchar(c);
 	_putchar('\n');
 }
 
